05e904f2c77cad2ce167c33872599d72cdf3c311
Alef <alefsantos8730@gmail.com>
2026-08-26 20:20:46 -0300

    feat: Album Model

--- app/Http/Controllers/AlbumController.php
+++ app/Http/Controllers/AlbumController.php
@@ -2,25 +2,89 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Album;
+use App\Models\Artist;
+use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class AlbumController extends Controller
 {
-    public function index() {
-        $albuns = [
-            ['nome' => "The Ultimate Collection", "artista" => "Michael Jackson", "ano_lancamento" => 1980],
-            ['nome' => "As Vozes Vol 1", "artista" => "Péricles", "ano_lancamento" => 2012],
-            ['nome' => "As Vozes Vol 2", "artista" => "Péricles", "ano_lancamento" => 2012],
-            ['nome' => "Swag", "artista" => "Justin Bieber", "ano_lancamento" => 2015],
-        ];
+    private string $messageInternalServerError = "Falha no servidor!Tente novamente mais tarde.";
 
-        return response()->json($albuns);
+    public function index() : JsonResponse {
+        try {
+            $albums = Album::all(); // Retorna  todos os registros da tabela
+
+            return response()->json(['message'=>'Albums listados com sucesso!', $albums],200);
+        } catch(Exception $ex){
+            return response()->json(['erro'=>$this->messageInternalServerError],500);
+        }
     }
 
-    public function show($id) {
-        return response()->json([
-            "status" => 200,
-            "mensagem" => "Iniciando busca pelo album $id"
-        ]);
+    public function show(int $id) : JsonResponse {
+        try {
+            $album = Album::findOrFail($id);
+
+            return response()->json(['message' => 'Almbum encontrado!', $album], 200);
+        }catch(Exception $ex) {
+            if($ex instanceof ModelNotFoundException) {
+                return response()->json(['erro' => 'Album não encontrado!'], 404);
+            }
+            return response()->json(['erro' => $this->messageInternalServerError], 500);
+        } 
+    }
+
+    public function store(Request $request) : JsonResponse {
+        try {
+            Artist::findOrFail($request->input("artist_id")); // Verifica se o artista existe
+            Album::create($request->all());
+
+            return response()->json(['message' => 'Album adicionado com sucesso!'], 201);
+        }catch(Exception $ex) {
+            // Tratamento caso haja algum campo obrigatório faltando
+            if($ex->getCode() == "HY000"){ 
+                return response()->json(['erro'=> "Preencha os dados obrigatórios!"], 400);
+            }
+
+            //Tratamento caso o artista referenciado não exista
+            if($ex instanceof ModelNotFoundException){
+                return response()->json(['erro'=> "Artista referenciado não encontrado!"], 404);
+            }
+            return response()->json(['erro' => $this->messageInternalServerError], 500);
+        }
+    }
+
+    public function update(Request $request, int $id) : JsonResponse {
+        try {
+            $album = Album::findOrFail($id);
+            $album->update($request->all());
+
+            return response()->json(['message'=>'Album atualizado com sucesso!'], 200);
+        }catch(Exception $ex) {
+            if($ex->getCode() == "HY000"){ 
+                return response()->json(['erro'=> "Preencha os dados obrigatórios!"], 400);
+            }
+
+            if($ex instanceof ModelNotFoundException){
+                return response()->json(['erro'=> "Album não encontrado!"], 404);
+            }
+            return response()->json(['erro'=> $this->messageInternalServerError], 500);
+        }
+    }
+    public function destroy(int $id) {
+        try {
+            $album = Album::findOrFail($id);
+
+            $album->delete();
+
+            return response()->json(['message' => "Album deletado com sucesso!"], 200);
+        }catch(Exception $ex) {
+            if($ex instanceof ModelNotFoundException){
+                return response()->json(['erro'=> "Album não encontrado!"], 404);
+            }
+            return response()->json(['erro'=> $this->messageInternalServerError], 500);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,25 +1,34 @@
 <?php
 
+use App\Http\Controllers\AlbumController;
 use App\Http\Controllers\ArtistController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MusicaController;
 use App\Http\Controllers\PlaylistController;
 
 Route::prefix('musicas')->group(function(){
-    Route::get('/', [MusicaController::class, 'index']);
     Route::post('/armazenar', [MusicaController::class, 'store']);
+    Route::get('/', [MusicaController::class, 'index']);
 });
 
 Route::prefix('playlists')->group(function(){
-    Route::get('/', [PlaylistController::class, 'index']);
     Route::post('/criar', [PlaylistController::class, 'store']);
+    Route::get('/', [PlaylistController::class, 'index']);
     Route::delete('/delete/{id}', [PlaylistController::class, 'destroy']);
 });
 
 Route::prefix('artists')->group(function(){
+    Route::post("/create-artist", [ArtistController::class, 'store']);
     Route::get('/', [ArtistController::class, 'index']);
     Route::get('/search-artist/{id}', [ArtistController::class, 'show']);
     Route::patch('/update-artist/{id}', [ArtistController::class, 'update']);
     Route::delete('/delete-artist/{id}', [ArtistController::class, 'destroy']);
-    Route::post("/create-artist", [ArtistController::class, 'store']);
+});
+
+Route::prefix('albums')->group(function () {
+    Route::post('/create-album', [AlbumController::class, 'store']);
+    Route::get('/list-albums', [AlbumController::class, 'index']);
+    Route::get('/list-album/{id}', [AlbumController::class, 'show']);
+    Route::patch('/update-album/{id}', [AlbumController::class, 'update']);
+    Route::delete('/delete-album/{id}', [AlbumController::class, 'destroy']);
 });
